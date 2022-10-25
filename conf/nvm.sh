a2ec852a5227d49cdb36b3a2649d253556a8092d
mkromp <matthias.krompass@adbv-a.bayern.de>
2022-10-25 20:47:55 +0200

    init from Laravel-Template

---
+++ conf/nvm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "${NVM_DIR}/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && source "${NVM_DIR}/bash_completion"
---
+++ nvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env 
+bashexport NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" 
+# This loads nvm
